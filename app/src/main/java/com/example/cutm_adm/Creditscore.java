4f55feed628eaa3b3c5ee9ec9a86338085506b12
Avinash85kr <112809709+Avinash85kr@users.noreply.github.com>
2023-04-05 12:18:25 +0530

    minor changes in dashboard

---
+++ app/src/main/java/com/example/cutm_adm/Creditscore.java
@@ -0,0 +1,14 @@
+package com.example.cutm_adm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Creditscore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditscore);
+    }
+}
